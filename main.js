dd77a114c7be67a75f77eb1563ff2509d3975831
timtjoe <ttjoe0@gmail.com>
2026-08-07 09:47:34 +0000

    normalize and parse HTTP headers

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const { default: parse } = require('./src/parse');
-
+const { default: parser } = require('./src/parser');
 const lines = require('fs').readFileSync(0, 'utf8').split('\n');
 
-function isVersion(s) {
-  // TODO: return true only if s matches /^HTTP\/\d+\.\d+$/
-  return s.startsWith("HTTP/");
-}
-
-parse.request(lines)
+// parser.request(lines);
+parser.headers(lines);
